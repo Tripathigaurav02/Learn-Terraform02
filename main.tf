ec9ae102a33fb4df3ff2a79aee04ba694eea23f7
Gaurav Tripathi <bussinessforgaurav@gmail.com>
2026-04-25 10:09:13 +0000

    Intial commit- setting up terraform

---
+++ main.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example2" {
+  ami           = "ami-0ec10929233384c7f"  # Ubuntu (Mumbai region)
+  instance_type = "t3.micro"
+tags = {
+    Name = "My-First-Instance"
+  }
+}
+
+resource "aws_instance" "example3" {
+ami           = "ami-0ec10929233384c7f"  # Ubuntu (Mumbai region)
+  instance_type = "t3.micro"
+tags = {
+    Name = "My-Second-Instance"
+  }
+}
+
+resource "aws_instance" "example4" {
+ami           = "ami-0ec10929233384c7f"  # Ubuntu (Mumbai region)
+  instance_type = "t3.micro"
+tags = {
+    Name = "My-Third-Instance"
+  }
+}
